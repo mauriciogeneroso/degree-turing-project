0b9cb0f0a9dfbff7ff523114195b675b6a941fe5
Maurício Generoso <mauro_generoso@hotmail.com>
2017-11-01 20:44:01 -0200

    Criado projeto

---
+++ TuringMachine/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TuringMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbValue1 = new System.Windows.Forms.TextBox();
+            this.tbValue2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbValue1
+            // 
+            this.tbValue1.Location = new System.Drawing.Point(104, 18);
+            this.tbValue1.Name = "tbValue1";
+            this.tbValue1.Size = new System.Drawing.Size(248, 20);
+            this.tbValue1.TabIndex = 0;
+            // 
+            // tbValue2
+            // 
+            this.tbValue2.Location = new System.Drawing.Point(104, 54);
+            this.tbValue2.Name = "tbValue2";
+            this.tbValue2.Size = new System.Drawing.Size(248, 20);
+            this.tbValue2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Primeiro valor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Segundo valor:";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(374, 16);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "Iniciar";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.Location = new System.Drawing.Point(374, 55);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(75, 23);
+            this.btExit.TabIndex = 5;
+            this.btExit.Text = "Sair";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 257);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbValue2);
+            this.Controls.Add(this.tbValue1);
+            this.Name = "Form1";
+            this.Text = "Maquina de Turing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbValue1;
+        private System.Windows.Forms.TextBox tbValue2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btExit;
+    }
+}
+
---
+++ TuringMachine/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuringMachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            String value1 = tbValue1.Text;
+            String value2 = tbValue2.Text;
+
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+        }
+    }
+}
